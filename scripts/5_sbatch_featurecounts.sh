3bd6841f80bc98f9b887d33f53121704fde5a8a6
soom12 <soo.m@northeastern.edu>
2024-08-13 14:34:46 -0400

    tweaks to featurecounts script

--- scripts/5_sbatch_featurecounts.sh
+++ scripts/5_sbatch_featurecounts.sh
@@ -4,10 +4,10 @@
 #SBATCH --time=02:00:00
 #SBATCH -N 1
 #SBATCH -n 4
-#SBATCH --output=<>
-#SBATCH --error=<>
+#SBATCH --output=/work/geisingerlab/Mark/rnaSeq/2024-08-12_wt-dLirL_rnaseq/logs/%x-%j.log
+#SBATCH --error=/work/geisingerlab/Mark/rnaSeq/2024-08-12_wt-dLirL_rnaseq/logs/%x-%j.err
 #SBATCH --mail-type=END
-#SBATCH --mail-user=<>
+#SBATCH --mail-user=soo.m@northeastern.edu
 
 echo "Loading environment and tools"
 module load subread/2.0.6
